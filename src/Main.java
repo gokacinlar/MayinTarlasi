7177f43975325d1a5db26c952a2463482c56bbde
gokacinlar <gokacinlarthedissident@gmail.com>
2023-12-21 22:33:55 +0300

    Added description.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * Java dilinde yazılmış olan bir mayın tarlası oyunudur.
+         * Yazar: @gokacinlar (with help of dear friend GPT 3.5 (:
+         * Kullanım: Repoyu git clone ile klonlayarak, IntelliJ IDE'sinde açabilirsiniz.
+         */
+
+
         MineSweeper game = new MineSweeper(3, 3);
         game.printMineLocations();
         game.playGame();
